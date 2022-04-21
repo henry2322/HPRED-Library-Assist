175c78b5f10efda97f4853a50c849a4eb7116350
DathanL37 <83264760+DathanL37@users.noreply.github.com>
2022-04-20 18:20:35 -0700

    Update BooksDBCommand.py

--- BooksDBCommand.py
+++ BooksDBCommand.py
@@ -3,12 +3,22 @@ import csv
 
 libDB = sqlite3.connect('library.db')
 
-def initDB():
+def initBookDB():
     crsr = libDB.cursor()
     crsr.execute("""CREATE Table User (Title varchar(255), Author varchar(255), Isbn int, Available int, Total int)""")
     a_file = open("book.csv")
     rows = csv.reader(a_file)
     crsr.executemany("INSERT INTO User VALUES (?, ?, ?, ?, ?)", rows)
+
+def endBookDB():
+    crsr = libDB.cursor()
+    a_file = open("book.csv")
+    wrtr = csv.writer(a_file)
+    crsr.execute("SELECT * FROM User;")
+    ans = crsr.fetchall()
+    for i in ans:
+        wrtr.writerow(i[0] + "," + i[1] + "," + str(i[2]) + "," + str(i[3]) + "," + str(i[4]))
+
 
 def searchBook(searching, searchMode):
     crsr = libDB.cursor()
@@ -44,8 +54,8 @@ def addBook(isbn,title,author,available,total):
         v = crsr.fetchone()
         crsr.execute("UPDATE User SET Total=" + str(v[3] + total) + "WHERE Isbn=" + isbn + ";")
     else:
-        crsr.execute("INSERT INTO User VALUES (Title,Author,Isbn,Availabe,Total) (" + title + "," + author + "," + str(isbn) +
-                     "," + str(available) + str(total) + ");")
+        crsr.execute("INSERT INTO User VALUES (Title,Author,Isbn,Availabe,Total) (" + title + "," + author + "," +
+                     str(isbn) + "," + str(available) + str(total) + ");")
     crsr.commit()
     crsr.close()
 
@@ -55,4 +65,3 @@ def delBook(isbn):
         crsr.execute("DELETE FROM User WHERE Isbn=" + isbn + ";")
         crsr.commit()
     crsr.close()
-
