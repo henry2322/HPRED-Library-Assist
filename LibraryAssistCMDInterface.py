6919ace37a0e29fdcd8aaad88735271402e6fe5b
DathanL37 <83264760+DathanL37@users.noreply.github.com>
2022-04-20 19:10:00 -0700

    Update LibraryAssistCMDInterface.py

--- LibraryAssistCMDInterface.py
+++ LibraryAssistCMDInterface.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         elif userinput[0] == "display":
             pass
         else:
-            if True:
+            if user.isSuperUser():
                 if userinput[0] == "removeUser" and len(userinput) == 2:
                     user.removeUser(userinput[1])
                 elif userinput[0] == "createUser" and len(userinput) == 2:
