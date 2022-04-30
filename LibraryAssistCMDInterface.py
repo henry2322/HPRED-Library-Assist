d24c2a3a347586eb8093a7b8b5bd2fb7992b9e1b
DathanL37 <83264760+DathanL37@users.noreply.github.com>
2022-04-29 18:03:05 -0700

    Update LibraryAssistCMDInterface.py

--- LibraryAssistCMDInterface.py
+++ LibraryAssistCMDInterface.py
@@ -10,23 +10,23 @@ if __name__ == "__main__":
         if userinput[0] == "exit":
             break
         elif userinput[0] == "search" and len(userinput) == 3:
-            pass
+            user.searchBook(userinput[1], userinput[2])
         elif userinput[0] == "holds":
             pass
         elif userinput[0] == "requestHold":
             pass
-        elif userinput[0] == "display":
-            pass
+        elif userinput[0] == "display" and len(userinput) == 2:
+            user.displayBook(userinput[1])
         else:
             if user.isSuperUser():
                 if userinput[0] == "removeUser" and len(userinput) == 2:
                     user.removeUser(userinput[1])
                 elif userinput[0] == "createUser" and len(userinput) == 2:
                     user.createUser(userinput[1])
-                elif userinput[0] == "addBook" and len(userinput) == 6:
-                    pass
+                elif userinput[0] == "addBook" and len(userinput) == 5:
+                    user.addBook(userinput[1], userinput[2], userinput[3], userinput[4])
                 elif userinput[0] == "removeBook" and len(userinput) == 2:
-                    pass
+                    user.removeBook(userinput[1])
 
             else:
                 print("Invalid Input")
